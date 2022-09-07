46875670ca11202ec892e47ae607a399d1da967e
Harsiddh Dave <harsiddhdave44@outlook.com>
2022-09-07 23:54:45 +0530

    Corrected an error in the leap year logic

--- 01-flow-of-programs/checkLeapYear.java
+++ 01-flow-of-programs/checkLeapYear.java
@@ -1,11 +1,29 @@
+/*
+Reference: https://www.quora.com/Why-are-centuries-in-the-Gregorian-calendar-divided-by-400-to-find-out-if-the-year-is-a-leap-year/answer/Shiva-Bala-Rakavi
+*/
+
 public class checkLeapYear {
     public static void main(String[] args) {
-        int year = 2000;
+        int year = 1600;
         checkifLeapYear(year);
     }
 
     static void checkifLeapYear(int year) {
-        if (year % 400 == 0 || year % 4 == 0) {
+        boolean isLeapYear = false;
+        if (year % 4 == 0) {
+            // check if the year is century
+            if (year % 100 == 0) {
+                // check if year is divided by 400
+                // then it is a leap year
+                if (year % 400 == 0)
+                    isLeapYear = true;
+            }
+            // if the year is not century
+            else
+                isLeapYear = true;
+        }
+
+        if (isLeapYear) {
             System.out.println(year + " is a leap year");
         } else {
             System.out.println(year + " is not a leap year");
